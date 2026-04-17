90a284c896c6015b2ebed845560b38a10b30fdcb
Paulo Longen <paulo@yelden.fund>
2026-04-17 06:03:06 -0400

    auto: daily score update 17/04/2026

--- scripts/agent_data.php
+++ scripts/agent_data.php
@@ -7,15 +7,15 @@
   "agent_type": "TRADING",
   "erc_id": "27703",
   "stake_yld": 50,
-  "sistema_score": 111,
-  "accumulated_score": 111,
+  "sistema_score": 109,
+  "accumulated_score": 109,
   "score_onchain": 111,
-  "ema": 405.8,
-  "sf": 0.4975,
-  "cf": 0.5523,
-  "s_raw": 19.9,
+  "ema": 373.5,
+  "sf": 0.4759,
+  "cf": 0.6124,
+  "s_raw": 19.04,
   "stage": "EXPERIMENTAL",
-  "total_trades_lifetime": 61,
+  "total_trades_lifetime": 75,
   "win_rate_lifetime": 0.601,
   "sharpe_lifetime": 1.908,
   "total_profit_lifetime": 1103.5,
@@ -35,7 +35,7 @@
       "accumulated": 654
     }
   ],
-  "last_updated": "2026-04-16T06:01:35.722864",
-  "last_tx": "7de342d0b72e3463e25f2b4e8e1f7985e3081b9429b0916e6d2ce228b3d5ebe6",
+  "last_updated": "2026-04-17T06:02:06.366856",
+  "last_tx": "62bfb772b75b493c52706fd4ccccd0a633365654d9273cb8bce55aa14609bb79",
   "scorer_version": "YAAF_v4.1"
 }
